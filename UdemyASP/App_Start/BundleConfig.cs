f2b9e1090df000df33ab330b333e07676cfce61c
Neko <Neko@TABLET-AQ7D2IAM>
2017-10-05 17:47:03 +0200

    section 1

--- UdemyASP/App_Start/BundleConfig.cs
+++ UdemyASP/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace UdemyASP
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-lumen.css",
                       "~/Content/site.css"));
         }
     }
